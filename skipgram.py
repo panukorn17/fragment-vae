5ccea31479c233fd5836740f5f6b53d1cec6bbbe
abhi1nandy2 <nandyabhilash@gmail.com>
2022-08-15 12:36:01 +0100

    first commit

---
+++ skipgram.py
@@ -0,0 +1,13 @@
+import time
+import numpy as np
+from gensim.models import Word2Vec
+from sklean.cluester import Kmeans
+from utils.filesystem import save_pickle, load_pickle
+
+SOS_TOKEN = '<SOS>'
+PAD_TOKEN = '<PAD>'
+EOS_TOKEN = '<EOS>'
+TOKENS = [SOS_TOKEN, PAD_TOKEN, EOS_TOKEN]
+
+class Vocab:
+    def __init__(self, data):
